f327e3cc5dad601f0ed3b286c0977a2c0dff631f
Guilhem <guilhem.vasselin@gmail.com>
2020-01-09 15:22:33 +0100

    Amelioration fonction setActivePastille

--- src/assets/script.js
+++ src/assets/script.js
@@ -9,9 +9,10 @@ function setActiveStep(id){
     element.classList.add("active");
 }
 
-//Ajoute la class css active pour savoir sur quel page on se trouve pour le bouton suivant
-function setActiveNext(){
-    
+//Ajoute la class css active pour savoir sur quel page on se trouve
+//Remarque : n = 1 pour suivant et n = -1 pour precedent
+function setActivePastille(n){
+    console.log("setActivePastille");
     let activeElement;
     let allDiv = document.getElementsByTagName("div");
     for (var i = 0; i < allDiv.length; i++) {
@@ -20,20 +21,12 @@ function setActiveNext(){
             break;
         }
     }
-    activeElement.classList.remove("active");
-    activeElement.nextSibling.classList.add("active");
-}
-
-//Ajoute la class css active pour savoir sur quel page on se trouve pour le bouton precedent
-function setActivePrev(){
-    let activeElement;
-    let allDiv = document.getElementsByTagName("div");
-    for (var i = 0; i < allDiv.length; i++) {
-        if(allDiv[i].classList.contains("active")){
-            activeElement = allDiv[i];
-            break;
-        }
+    if(n==1){
+        activeElement.classList.remove("active");
+        activeElement.nextSibling.classList.add("active");
     }
-    activeElement.classList.remove("active");
-    activeElement.previousSibling.classList.add("active");
+    else if(n==-1){
+        activeElement.classList.remove("active");
+        activeElement.previousSibling.classList.add("active");
+    }
 }
