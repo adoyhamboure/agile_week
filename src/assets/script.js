01692f4cc27d5e339d0393911950ef1d4bb4e81e
Guilhem <guilhem.vasselin@gmail.com>
2020-01-09 13:30:43 +0100

    Ajout animation pastille

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,40 +1,39 @@
-//Fonction OnClick utilisé pour les pastilles en bas lors de clique sur PASTILLE
- 
+
+//Fonction d'animation pour les pastilles d'etape lors du clique sur celle-ci
 function setActiveStep(id){
-    //console.log(id);
     let old = document.getElementsByClassName("active");
     if(old.length > 0 ){
         old = old[0].classList.remove("active");
     }
-
     var element = document.getElementById(id);
     element.classList.add("active");
 }
 
-//Fonction OnClick utilisé pour les pastilles en bas lors de clique sur FLECHE SUIVANT
-
-function setActiveNext(id){
-    console.log("suivant");
-    let old = document.getElementsByClassName("active");
-    if(old.length > 0 ){
-        old = old[0].classList.remove("active");
+//Ajoute la class css active pour savoir sur quel page on se trouve pour le bouton suivant
+function setActiveNext(){
+    
+    let activeElement;
+    let allDiv = document.getElementsByTagName("div");
+    for (var i = 0; i < allDiv.length; i++) {
+        if(allDiv[i].classList.contains("active")){
+            activeElement = allDiv[i];
+            break;
+        }
     }
-
-    var element = document.getElementById(id);
-    element.classList.add("active");
+    activeElement.classList.remove("active");
+    activeElement.nextSibling.classList.add("active");
 }
 
-//Fonction OnClick utilisé pour les pastilles en bas lors de clique sur FLECHE PRECEDENT
-
-function setActivePrev(activeElement){
-    console.log(activeElement.id);
-    var element = document.getElementById(activeElement[0].id);
-    //console.log(element.id);
-    //console.log(document.getElementById(id).previousElementSibling.innerHTML);
-    let old = document.getElementsByClassName("active");
-    if(old.length > 0 ){
-        old = old[0].classList.remove("active");
+//Ajoute la class css active pour savoir sur quel page on se trouve pour le bouton precedent
+function setActivePrev(){
+    let activeElement;
+    let allDiv = document.getElementsByTagName("div");
+    for (var i = 0; i < allDiv.length; i++) {
+        if(allDiv[i].classList.contains("active")){
+            activeElement = allDiv[i];
+            break;
+        }
     }
-
-    element.classList.add("active");
+    activeElement.classList.remove("active");
+    activeElement.previousSibling.classList.add("active");
 }
