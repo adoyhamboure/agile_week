e764d8d026115aadd9e1b983cca9b1ddb55d3852
David <david.jacquelin@outlook.com>
2020-01-09 13:04:41 +0100

    TicTac update (not working yet)

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,7 +1,7 @@
-//Fonction OnClick utilisé pour les pastilles 
+//Fonction OnClick utilisé pour les pastilles en bas lors de clique sur PASTILLE
  
 function setActiveStep(id){
-    console.log(id);
+    //console.log(id);
     let old = document.getElementsByClassName("active");
     if(old.length > 0 ){
         old = old[0].classList.remove("active");
@@ -9,6 +9,32 @@ function setActiveStep(id){
 
     var element = document.getElementById(id);
     element.classList.add("active");
+}
 
-   
+//Fonction OnClick utilisé pour les pastilles en bas lors de clique sur FLECHE SUIVANT
+
+function setActiveNext(id){
+    console.log("suivant");
+    let old = document.getElementsByClassName("active");
+    if(old.length > 0 ){
+        old = old[0].classList.remove("active");
+    }
+
+    var element = document.getElementById(id);
+    element.classList.add("active");
+}
+
+//Fonction OnClick utilisé pour les pastilles en bas lors de clique sur FLECHE PRECEDENT
+
+function setActivePrev(activeElement){
+    console.log(activeElement.id);
+    var element = document.getElementById(activeElement[0].id);
+    //console.log(element.id);
+    //console.log(document.getElementById(id).previousElementSibling.innerHTML);
+    let old = document.getElementsByClassName("active");
+    if(old.length > 0 ){
+        old = old[0].classList.remove("active");
+    }
+
+    element.classList.add("active");
 }
